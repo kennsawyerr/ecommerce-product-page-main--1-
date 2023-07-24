38af47c1c7a26db1d701ad488eb6581e7fdafb32
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-07-24 23:34:54 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// first, add a button icon to ul.
-//2, then 
+const menuBtn = document.getElementById("menuBtn");
+
+const navOpt = document.querySelector
+("#navOpt");
+
+menuBtn.addEventListener("click", ()=>{
+    navOpt.classList.toggle("navOpt")
+})
