3262b0ba2941f4b19705dec9eb4a2f250c60557d
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-07-28 23:58:16 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 const menuBtn = document.getElementById("menuBtn");
 
+
 const navOpt = document.querySelector
 ("#navOpt");
 
 menuBtn.addEventListener("click", ()=>{
-    navOpt.classList.toggle("navOpt")
+    navOpt.classList.toggle("navShow")
 })
+
+
+
+function Drop() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+
+let count = 0;
+
+const btns =document.querySelectorAll('.btn');
+const value =document.querySelector('#qtyNum');
+
+
+
+
+
+btns.forEach(function (btn){
+btn.addEventListener("click", function (e)
+{const timz = e.currentTarget.classList;
+    if (timz.contains("minus")){
+        count--; }
+    else if( timz.contains("plus")){
+        count++
+    }
+    else(count=0)
+   
+    value.textContent=count;
+
+});
+});
+
+//get all values with query selector
+
+
+
