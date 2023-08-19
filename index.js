2d420164a56a61cd8fd401070762304dd4a7d6f2
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-08-19 13:21:21 +0100

    Changes to be committed: 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const slideImages=[
-    {
+const slideImages = [{
         Id: 1,
         img: "images/image-product-1.jpg",
-    
+
     },
 
     {
         Id: 2,
         img: "images/image-product-2.jpg",
-    
-    }
-,
+
+    },
     {
         Id: 3,
         img: "images/image-product-3.jpg",
-    
-    }
-,
+
+    },
     {
         Id: 4,
         img: "images/image-product-4.jpg",
-    
+
     }
 
 
@@ -28,58 +25,68 @@ const slideImages=[
 
 
 // slideshow data
-const IMG =document.getElementById("product-img");
+const IMG = document.getElementById("product-img");
 const prevBtn = document.querySelector(".prev")
 const nextBtn = document.querySelector(".next")
 let slideImagesIDValue = 2;
-
-
-// ==========
 // navigation
 const menuBtn = document.getElementById("menuBtn");
 const overlay = document.querySelector(".overlay");
+const menuCloseBtn = document.querySelector(".close-btn");
+//clicking on 
+const clickedImgContainer = document.querySelector(".clickableImgContainer")
+const navOpt = document.querySelector(".navMenu");
+const closeClickableImg = document.querySelector(".closeClickableImg");
 
 
-const clickedImg =document.querySelector(".clickableImgContainer")
-
-
-
+//
+//
+//FUNCTIONS=====
 // 
-function overlayStyle(){
+function overlayStyle() {
     overlay.style.visibility = "visible";
-   
+}
+
+function noOverlayStyle() {
+    overlay.style.visibility = "hidden";
 }
 // 
-// 
-// absolute image
-
-function ClickFullScreen(){
+// shoe images but on large screen
+function ClickFullScreen() {
     overlayStyle();
-     clickedImg.style.visibility="visible";
+    clickedImgContainer.style.visibility = "visible";
 }
 
 
+closeClickableImg.addEventListener("click", function () {
+    clickedImgContainer.style.visibility = "hidden";
+    noOverlayStyle()
+})
+
+
 // 
 // 
 
 
-const navOpt = document.querySelector
-(".navMenu");
 
-menuBtn.addEventListener("click", ()=>{
+
+menuBtn.addEventListener("click", () => {
     navOpt.classList.toggle("navShow");
     overlayStyle();
- 
+})
+
+menuCloseBtn.addEventListener("click", function(){
+     navOpt.classList.remove("navShow");
+     noOverlayStyle();
 })
 
 // removing navbar if other parts of the screen  is clicked
-window.addEventListener("click",function(event){
-    if(event.target != menuBtn){navOpt.classList.remove("navShow");
-    // overlay.style.visibility = "hidden";
-
-}
-
-} )
+// window.addEventListener("click", function (event) {
+//     if (event.target != menuBtn) {
+//         navOpt.classList.remove("navShow");
+//         // overlay.style.visibility = "hidden";
+//     }
+// })
 
 
 // Dropdown menu
@@ -91,38 +98,27 @@ function Drop() {
 // Slide show buttons 
 
 
-function showPerson(){
+function showPerson() {
     let item = slideImages[slideImagesIDValue];
     IMG.src = item.img;
-   
-      }
 
-      nextBtn.addEventListener("click", function(){
-        slideImagesIDValue++
-        if( slideImagesIDValue > slideImages.length-1){
-            slideImagesIDValue= 0;
-        }
-        return showPerson(slideImagesIDValue)
-    })
-    
+}
 
-    
-    
-    prevBtn.addEventListener("click", function(){
-        slideImagesIDValue--
-        if( slideImagesIDValue < 0 ){
-            slideImagesIDValue= slideImages.length-1;
-        }
-        return showPerson(slideImagesIDValue)
-    })
+nextBtn.addEventListener("click", function () {
+    slideImagesIDValue++
+    if (slideImagesIDValue > slideImages.length - 1) {
+        slideImagesIDValue = 0;
+    }
+    return showPerson(slideImagesIDValue)
+})
 
 
 
-    // const displayimages = document.querySelectorAll(".smallImages");
 
-    // displayimages.forEach(element => {
-    //     element.addEventListener("click", ()=> {
-            
-    //      this.classList.toggle("full")
-    //     })
-    // })
+prevBtn.addEventListener("click", function () {
+    slideImagesIDValue--
+    if (slideImagesIDValue < 0) {
+        slideImagesIDValue = slideImages.length - 1;
+    }
+    return showPerson(slideImagesIDValue)
+})
